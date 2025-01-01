eb2a80cefab1c8247bdead76814b06c5e844c779
albertcai101 <albertcai.paloalto@gmail.com>
2025-01-01 12:33:55 -1000

    copy button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
     Avatar,
     AvatarFallback,
@@ -28,16 +30,18 @@ export default function Home() {
                 </ol>
 
                 <div className="flex gap-4 items-center flex-col sm:flex-row">
-                    <a 
-                        href="mailto:albertcai.paloalto@gmail.com"
+                    <button
+                        onClick={() => {
+                            navigator.clipboard.writeText("albertcai.paloalto@gmail.com");
+                        }}
                         className="flex items-center gap-2 hover:underline hover:underline-offset-4 font-[family-name:var(--font-geist-mono)]"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <rect width="20" height="16" x="2" y="4" rx="2"/>
-                            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/>
+                            <rect width="14" height="14" x="8" y="8" rx="2" ry="2"/>
+                            <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/>
                         </svg>
                         albertcai.paloalto@gmail.com
-                    </a>
+                    </button>
                 </div>
             </main>
         </div>
